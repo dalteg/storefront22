a41255c60925f8bc6541a6fd57d2e544d1dfdb3b
dalteg <dennisaltegaz@gmail.com>
2026-03-19 16:01:50 +0300

    custom SQL added

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 # Create your views here.
 # request -> response
 # request handler
 # action
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
-def say_hello(request):
-    x = calculate()
+
+'''def say_hello(request):
+    product = Product.objects.filter(pk=1).first()  # alternatively also 
+    # pexists = Product.objects.filter(pk=1).exists()
+alternatively use 
+    try: # if  a product  is non existent you  can use the  try catch method or the  above
+        product = Product.objects.get(pk=1) 
+    except ObjectDoesNotExist:
+        pass
+    
     return render(request, 'hello.html', {'name' :'kagechi'})
+'''
+
+# filtering 
+def say_hello(request):
+    queryset = Product.objects.filter(unit_price__range=(20,30))
+    return render(request, 'hello.html', {'name' :'kagechi', 'product':list(queryset)})
--- storefront22/settings.py
+++ storefront22/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'storefront22.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront22',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD': '1323'
     }
 }
 
