42ff237e269b6cddd575c2ab401d2e0a2c2c944d
dalteg <dennisaltegaz@gmail.com>
2026-04-30 07:23:16 +0300

    pytest

---
+++ store/tests/test_collections.py
@@ -0,0 +1,16 @@
+from rest_framework import status
+from rest_framework.test import APIClient
+import pytest
+
+@pytest.mark.django_db
+class TestCreatCollection:
+    @pytest.mark.skip
+    def test_if_user_is_anonymous_returns_401(self):
+        #Arrange
+
+        #Act
+        client = APIClient()
+        response = client.post('/store/collections/', {'title':'a'})
+        # assert
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
