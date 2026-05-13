d2ba6fc7b624abe051a27a9a9d2497dd90816687
dalteg <dennisaltegaz@gmail.com>
2026-05-13 08:34:02 +0300

    loaddata datadump.json

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py loaddata datadump.json
+
 
 # Temporary superuser creation
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'dennisaltegaz@gmail.com', 'admin1234')" | python manage.py shell
