c48939ebf4f3789543b1982c5297d76984baa4ce
dalteg <dennisaltegaz@gmail.com>
2026-05-13 10:54:25 +0300

    fix: exclude silk from loaddata in production

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 python manage.py migrate --settings=storefront22.settings.prod
-python manage.py loaddata datadump.json --settings=storefront22.settings.prod
+python manage.py loaddata datadump.json --settings=storefront22.settings.prod --exclude=silk
 python manage.py collectstatic --noinput
