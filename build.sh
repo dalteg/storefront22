b150478c6ab05e6302f16ca5baea67e1b7cb2836
dalteg <dennisaltegaz@gmail.com>
2026-05-13 12:44:16 +0300

    add seed_db to build command

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 
 pip install -r requirements.txt
 python manage.py migrate --settings=storefront22.settings.prod
-python manage.py loaddata datadump.json --settings=storefront22.settings.prod --exclude=silk
 python manage.py seed_db --settings=storefront22.settings.prod
 python manage.py collectstatic --noinput
