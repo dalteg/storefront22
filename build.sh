b0853dda72a03970877e57cf8fbbe3ead141805a
dalteg <dennisaltegaz@gmail.com>
2026-05-13 10:47:32 +0300

    fix: use prod settings for migrate and loaddata

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py migrate
+python manage.py migrate --settings=storefront22.settings.prod
+python manage.py loaddata datadump.json --settings=storefront22.settings.prod
 python manage.py collectstatic --noinput
