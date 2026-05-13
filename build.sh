29ebaf4d2103f959ca401a3927f53b9a35e044b4
dalteg <dennisaltegaz@gmail.com>
2026-05-13 08:10:52 +0300

    temp: create superuser

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+# Temporary superuser creation
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'dennisaltegaz@gmail.com', 'admin1234')" | python manage.py shell
