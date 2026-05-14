c2f971d2e710ccc1c67a3841476a7efce4fb6e33
dalteg <dennisaltegaz@gmail.com>
2026-05-14 07:56:23 +0300

    fix: remove createsuperuser from build

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ python manage.py migrate --settings=storefront22.settings.prod
 python manage.py seed_db --settings=storefront22.settings.prod
 python manage.py collectstatic --noinput
 
-python manage.py createsuperuser --noinput --username admin --email admin@admin.com --settings=storefront22.settings.prod
